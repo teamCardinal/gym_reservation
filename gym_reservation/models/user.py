7b975f5071170204712315e620f233e7c1fc61f6
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2020-09-11 10:18:42 -0400

    Add user database model

---
+++ gym_reservation/models/user.py
@@ -0,0 +1,13 @@
+from gym_reservation import db 
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    gym = db.Column(db.String(20), nullable=False)
+    membership_id = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.gym}', '{self.membership_id}')"
