73a34f8f7b2106810bce6364f8b5bab82ff29187
Aiman Rizvi <aimanxrizvi@gmail.com>
2020-09-10 22:07:18 -0500

    WIP-homepage

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
