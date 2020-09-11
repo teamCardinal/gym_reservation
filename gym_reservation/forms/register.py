d3834655ec5f0ce96b07d5e58e9813c0203663c6
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2020-09-11 12:56:54 -0400

    Add account validation methods for registration page

--- gym_reservation/forms/register.py
+++ gym_reservation/forms/register.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField 
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+
+from gym_reservation.models.user import User
 
 
 class RegistrationForm(FlaskForm):
@@ -29,3 +31,25 @@ class RegistrationForm(FlaskForm):
             validators=[DataRequired(), EqualTo("password")])
 
     submit = SubmitField("Sign Up")
+
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError("That username is taken. Please choose a different one.")
+
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError("That email is taken. Please use a different one.")
+
+    def validate_membership_id(self, membership_id):
+
+        user = User.query.filter_by(membership_id=membership_id.data).first()
+
+        if user:
+            raise ValidationError("That membership_id is taken. Please use a different one.")
