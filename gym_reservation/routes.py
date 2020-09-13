791408743711fd49b5febc2bc0ab85a0a02eaa59
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2020-09-13 01:28:27 -0400

    Cleanup

--- gym_reservation/routes.py
+++ gym_reservation/routes.py
@@ -81,7 +81,6 @@ def account(username):
 def user_sessions(username):
     if username == current_user.username:
         user_sessions = current_user.sessions
-        print(user_sessions)
         gym_sessions = route_helpers.convert_user_sessions_to_gym_sessions(user_sessions)
         return render_template("user_sessions.html", title="MySessions", sessions=gym_sessions)
     else:
