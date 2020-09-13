3456ce9a1333d006dbd3b24082b750f338b0c2d4
Mark Doughten <doughtm1@tcnj.edu>
2020-09-13 18:39:46 -0400

    Not used during the registration form, deleting because no longer used

--- run.py
+++ run.py
@@ -18,4 +18,4 @@ def create_tables():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='127.0.0.1', port=port)
